4de021405b62cc5e60dc87ae5607e658995bf408
Oleh Kozhushko <olkozhushko@gmail.com>
2019-06-23 23:37:27 +0300

    changed some stuff

--- src/app/Reducers/getDataReducer.js
+++ src/app/Reducers/getDataReducer.js
@@ -1,9 +1,9 @@
-import { FETCH_USER, FETCHED_USER, RECEIVE_ERROR } from "./constants";
+import { FETCH_USER, FETCHED_USER, RECEIVE_ERROR } from "../Actions/constants";
 
 const initialState = {
-    fetchedData: {},
+    fetchedData: [{title: "american pie", poster: "https://images.pexels.com/photos/2395264/pexels-photo-2395264.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}],
     isFetching: false,
-    isError: false
+    isErrored: false
 }
 
 const getDataReducers = (state = initialState, action) => {
@@ -12,21 +12,22 @@ const getDataReducers = (state = initialState, action) => {
             return {
                 ...state, 
                 isFetching: true,
-                isError: false
+                isErrored: false
             }
         case FETCHED_USER:
             return {
                 ...state,
                 fetchedData: action.data,
                 isFetching: false,
-                isError: false
+                isErrored: false
             }
         case RECEIVE_ERROR:
             return {
                 ...state,
-                isError: true
+                isErrored: true
             }
         default:
+            console.log(state);
             return state;
     }
 }
