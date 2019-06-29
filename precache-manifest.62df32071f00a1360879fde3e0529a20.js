af0bf8074a6dfe36fb527838b2c69d3b2316b3ca
Oleh Kozhushko <olkozhushko@gmail.com>
2019-06-29 15:06:56 +0300

    Updates

---
+++ precache-manifest.62df32071f00a1360879fde3e0529a20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab88ac59884cfad55bc6ec71b0e1fb25",
+    "url": "/moviefinder/index.html"
+  },
+  {
+    "revision": "880c727ad6f396ee5bcf",
+    "url": "/moviefinder/static/css/main.3ca0b949.chunk.css"
+  },
+  {
+    "revision": "3b8a261ad510ecc7951a",
+    "url": "/moviefinder/static/js/2.cf7492f5.chunk.js"
+  },
+  {
+    "revision": "880c727ad6f396ee5bcf",
+    "url": "/moviefinder/static/js/main.09db1175.chunk.js"
+  },
+  {
+    "revision": "31126f5698e1765fd403",
+    "url": "/moviefinder/static/js/runtime~main.afef7d54.js"
+  }
+]);
